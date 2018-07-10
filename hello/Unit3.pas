75db49877d8e1db7cb7ce076519ac777f1f6220b
tfcandy <40813233+tfcandy@users.noreply.github.com>
2018-07-10 15:23:42 +0700

    Button click
    say hello

---
+++ hello/Unit3.pas
@@ -0,0 +1,35 @@
+unit Unit3;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation, FMX.Edit;
+
+type
+  TForm3 = class(TForm)
+    Edit1: TEdit;
+    Label1: TLabel;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.fmx}
+{$R *.LgXhdpiPh.fmx ANDROID}
+
+procedure TForm3.Button1Click(Sender: TObject);
+begin
+ Label1.Text := 'Hello ' + Edit1.Text + ' !';
+end;
+
+end.
